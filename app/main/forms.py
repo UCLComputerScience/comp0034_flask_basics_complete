b467273f9e2899e1f7eecca5e5333531b62c10d6
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2020-01-31 10:17:26 +0000

    Added more database records.

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,6 +6,7 @@ from wtforms.validators import DataRequired, EqualTo, Email
 class SignupForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email address', validators=[DataRequired(), Email(message='Valid email address required')])
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Repeat Password')
     submit = SubmitField('Sign Up')
